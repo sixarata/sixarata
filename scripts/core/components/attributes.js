69b54a334998153468417c026267c2f2102b0b7b
John James Jacoby <johnjamesjacoby@me.com>
2024-06-11 15:39:01 -0500

    Attributes: first pass
    Much more to do & going on

---
+++ scripts/core/components/attributes.js
@@ -0,0 +1,38 @@
+/**
+ * The Attributes object.
+ *
+ * This object is responsible for the Attributes of any Component.
+ */
+export default class Attributes {
+
+	/**
+	 * Default attributes.
+	 */
+	defaults = new Map();
+
+	/**
+	 * Modified attributes.
+	 */
+	modified = new Map();
+
+	constructor( entries = {} ) {
+		this.defaults = new Map( entries );
+		this.reset( entries );
+	}
+
+	reset = () => {
+		return this.modified = this.defaults;
+	}
+
+	clear = () => {
+		return this.modified.clear();
+	}
+
+	set = ( key = '', value = {} ) => {
+		return this.modified.set( key, value );
+	}
+
+	get = ( key = '' ) => {
+		return this.modified.get( key );
+	}
+}
