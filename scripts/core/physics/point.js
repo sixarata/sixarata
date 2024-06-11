0fc507700f6ab0823bb4c40121826f14624473a4
John James Jacoby <johnjamesjacoby@me.com>
2024-06-11 10:50:06 -0500

    Point: var & inline docs

--- scripts/core/physics/point.js
+++ scripts/core/physics/point.js
@@ -7,23 +7,50 @@
 export default class Point {
 
 	/**
+	 * The X value.
+	 *
+	 * @var {number} x Default 0.
+	 */
+	x = 0;
+
+	/**
+	 * The Y value.
+	 *
+	 * @var {number} y Default 0.
+	 */
+	y = 0;
+
+	/**
+	 * The minimum tolerance for Point comparisons.
+	 *
+	 * @var {number} minTolerance Default 0.0001.
+	 */
+	minTolerance = 0.0001;
+
+	/**
 	 * Construct the Point.
 	 *
-	 * @param {Int} x
-	 * @param {Int} y
+	 * @param {Number} x
+	 * @param {Number} y
 	 */
-	constructor( x = 0, y = 0 ) {
+	constructor(
+		x = 0,
+		y = 0
+	) {
 		this.set( x, y );
 	}
 
 	/**
 	 * Set the coordinates of the Point.
 	 *
-	 * @param {Int} x
-	 * @param {Int} y
-	 * @returns {Point}
+	 * @param   {Number} x
+	 * @param   {Number} y
+	 * @returns {Point}  This Point, with new values.
 	 */
-	set = ( x = 0, y = 0 ) => {
+	set = (
+		x = 0,
+		y = 0
+	) => {
 		this.x = x;
 		this.y = y;
 
@@ -33,7 +60,7 @@ export default class Point {
 	/**
 	 * Reset the coordinates of the Point to 0.
 	 *
-	 * @returns {Point}
+	 * @returns {Point} The new Point.
 	 */
 	reset = () => {
 		return this.set( 0, 0 );
@@ -42,10 +69,15 @@ export default class Point {
 	/**
 	 * Add a Point to this Point.
 	 *
-	 * @param {Point} p
-	 * @returns {Point}
+	 * @param   {Point} p
+	 * @returns {Point} This Point, with new values.
 	 */
-	add = ( p = { x: 0, y: 0 } ) => {
+	add = (
+		p = {
+			x: 0,
+			y: 0
+		}
+	) => {
 		return this.set(
 			( this.x + p.x ),
 			( this.y + p.y ),
@@ -55,10 +87,12 @@ export default class Point {
 	/**
 	 * Add to this Point linearly.
 	 *
-	 * @param {Int} scale
-	 * @returns {Point}
+	 * @param   {Number} scale
+	 * @returns {Point}  This Point, with new values.
 	 */
-	addLinear = ( scale = 1 ) => {
+	addLinear = (
+		scale = 1
+	) => {
 		return this.add( {
 			x: scale,
 			y: scale,
@@ -68,10 +102,15 @@ export default class Point {
 	/**
 	 * Subtract a Point from this Point.
 	 *
-	 * @param {Point} p
-	 * @returns {Point}
+	 * @param   {Point} p
+	 * @returns {Point} This Point, with new values.
 	 */
-	sub = ( p = { x: 0, y: 0 } ) => {
+	sub = (
+		p = {
+			x: 0,
+			y: 0
+		}
+	) => {
 		return this.set(
 			( this.x - p.x ),
 			( this.y - p.y ),
@@ -81,10 +120,12 @@ export default class Point {
 	/**
 	 * Subtract from this Point linearly.
 	 *
-	 * @param {Int} scale
-	 * @returns {Point}
+	 * @param   {Number} scale
+	 * @returns {Point}  This Point, with new values.
 	 */
-	subLinear = ( scale = 1 ) => {
+	subLinear = (
+		scale = 1
+	) => {
 		return this.sub( {
 			x: scale,
 			y: scale,
@@ -94,10 +135,15 @@ export default class Point {
 	/**
 	 * Multiply this Point by a Point.
 	 *
-	 * @param {Point} p
-	 * @returns {Point}
+	 * @param   {Point} p
+	 * @returns {Point} This Point, with new values.
 	 */
-	multiply = ( p = { x: 0, y: 0 } ) => {
+	multiply = (
+		p = {
+			x: 0,
+			y: 0
+		}
+	) => {
 		return this.set(
 			( this.x * p.x ),
 			( this.y * p.y ),
@@ -107,10 +153,12 @@ export default class Point {
 	/**
 	 * Multiply this Point linearly.
 	 *
-	 * @param {Int} scale
-	 * @returns {Point}
+	 * @param   {Number} scale
+	 * @returns {Point}  This Point, with new values.
 	 */
-	multiplyLinear = ( scale = 1 ) => {
+	multiplyLinear = (
+		scale = 1
+	) => {
 		return this.multiply( {
 			x: scale,
 			y: scale,
@@ -120,10 +168,15 @@ export default class Point {
 	/**
 	 * Divide this Point by a Point.
 	 *
-	 * @param {Point} p
-	 * @returns {Point}
+	 * @param   {Point} p
+	 * @returns {Point} This Point, with new values.
 	 */
-	divide = ( p = { x: 0, y: 0 } ) => {
+	divide = (
+		p = {
+			x: 0,
+			y: 0
+		}
+	) => {
 		return this.set(
 			( this.x / p.x ),
 			( this.y / p.y ),
@@ -133,10 +186,12 @@ export default class Point {
 	/**
 	 * Divide this Point linearly.
 	 *
-	 * @param {Int} scale
-	 * @returns {Point}
+	 * @param   {Number} scale
+	 * @returns {Point}  This Point, with new values.
 	 */
-	divideLinear = ( scale = 1 ) => {
+	divideLinear = (
+		scale = 1
+	) => {
 		if ( scale ) {
 			return this.divide( {
 				x: scale,
@@ -150,10 +205,15 @@ export default class Point {
 	/**
 	 * Import a Point into this Point.
 	 *
-	 * @param {Point} p
-	 * @returns {Point}
+	 * @param   {Point} p
+	 * @returns {Point} This Point, with new values.
 	 */
-	import = ( p = { x: 0, y: 0 } ) => {
+	import = (
+		p = {
+			x: 0,
+			y: 0
+		}
+	) => {
 		return this.set(
 			p.x,
 			p.y,
@@ -163,7 +223,7 @@ export default class Point {
 	/**
 	 * Export this Point into a new Point.
 	 *
-	 * @returns {Point}
+	 * @returns {Point} A new Point.
 	 */
 	export = () => {
 		return new Point(
@@ -175,7 +235,7 @@ export default class Point {
 	/**
 	 * Calculate the square of this Point.
 	 *
-	 * @returns {Point}
+	 * @returns {Point} The square.
 	 */
 	square = () => {
 		return (
@@ -188,7 +248,7 @@ export default class Point {
 	/**
 	 * Get the length of this Point.
 	 *
-	 * @returns {Int}
+	 * @returns {Number} The length.
 	 */
 	length = () => {
 		return Math.sqrt( this.square() );
@@ -197,10 +257,15 @@ export default class Point {
 	/**
 	 * Get the square distance between Points.
 	 *
-	 * @param {Point} p
-	 * @returns {Int}
+	 * @param   {Point}  p
+	 * @returns {Number} The square distance.
 	 */
-	squareDistance = ( p = { x: 0, y: 0 } ) => {
+	squareDistance = (
+		p = {
+			x: 0,
+			y: 0
+		}
+	) => {
 		let dx = ( this.x - p.x ),
 			dy = ( this.y - p.y );
 
@@ -214,39 +279,54 @@ export default class Point {
 	/**
 	 * Get the normal distance between Points.
 	 *
-	 * @param {Point} p
-	 * @returns {Int}
+	 * @param   {Point}  p
+	 * @returns {Number} The distance.
 	 */
-	distance = ( p = { x: 0, y: 0 } ) => {
+	distance = (
+		p = {
+			x: 0,
+			y: 0
+		}
+	) => {
 		return Math.sqrt( this.squareDistance( p ) );
 	}
 
 	/**
 	 * Does a Point equal this Point?
 	 *
-	 * @param {Point} p
-	 * @returns {Boolean}
+	 * @param   {Point}   p
+	 * @returns {Boolean} True if equal.
 	 */
-	equals = ( p = { x: 0, y: 0 } ) => {
-		return ( this.distance( p ) < 0.0001 );
+	equals = (
+		p = {
+			x: 0,
+			y: 0
+		}
+	) => {
+		return ( this.distance( p ) < this.minTolerance );
 	}
 
 	/**
 	 * Is this Point empty?
 	 *
-	 * @returns {Boolean}
+	 * @returns {Boolean} True if empty.
 	 */
 	empty = () => {
-		return ( this.length() < 0.0001 );
+		return ( this.length() < this.minTolerance );
 	}
 
 	/**
 	 * Get the dot product of two Points.
 	 *
-	 * @param {Point} p
-	 * @returns {Int}
+	 * @param   {Point}  p
+	 * @returns {Number} The dot product.
 	 */
-	dot = ( p = { x: 0, y: 0 } ) => {
+	dot = (
+		p = {
+			x: 0,
+			y: 0
+		}
+	) => {
 		return (
 			( this.x * p.x )
 			+
@@ -257,11 +337,17 @@ export default class Point {
 	/**
 	 * Linearly progress this Point to a Point.
 	 *
-	 * @param {Point} p
-	 * @param {Int}   i
-	 * @returns {Point}
+	 * @param   {Point}  p
+	 * @param   {Number} i
+	 * @returns {Point}  This Point, with new values.
 	 */
-	lerp = ( p = { x: 0, y: 0 }, i = 1 ) => {
+	lerp = (
+		p = {
+			x: 0,
+			y: 0
+		},
+		i = 1
+	) => {
 		let x = ( ( p.x - this.x ) * i ) + this.x,
 			y = ( ( p.y - this.y ) * i ) + this.y;
 
