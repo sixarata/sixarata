b38c6e2d2def80c5cfc9794c259f1a2bdab98fe9
John James Jacoby <johnjamesjacoby@me.com>
2024-06-11 11:41:41 -0500

    Player: vars, docs
    Cludge retries to stop undefined problems

--- scripts/core/tiles/player.js
+++ scripts/core/tiles/player.js
@@ -2,7 +2,7 @@ import Settings from '../settings.js';
 import Game     from '../game.js';
 
 import { Tile } from './exports.js';
-import { Collision, Contact, Position, Velocity } from '../physics/exports.js';
+import { Collision, Contact, Orientation, Position, Velocity } from '../physics/exports.js';
 
 /**
  * The Player object.
@@ -12,13 +12,73 @@ import { Collision, Contact, Position, Velocity } from '../physics/exports.js';
 export default class Player extends Tile {
 
 	/**
+	 * The Player's retries.
+	 *
+	 * @var {Object} Default from Settings.
+	 */
+	retries = Settings.player.retries;
+
+	/**
+	 * The Player's health.
+	 *
+	 * @var {Object} Default from Settings.
+	 */
+	health = Settings.player.health;
+
+	/**
+	 * The Player's jumps.
+	 *
+	 * @var {Object} Default from Settings.
+	 */
+	jumps = Settings.player.jumps;
+
+	/**
+	 * The Player's contact.
+	 *
+	 * @var {Contact} Default new Contact().
+	 */
+	contact = new Contact();
+
+	/**
+	 * The Player's orientation.
+	 *
+	 * @var {Orientation} Default new Orientation().
+	 */
+	orientation = new Orientation();
+
+	/**
+	 * The Player's velocity.
+	 *
+	 * @var {Velocity} Default new Velocity().
+	 */
+	velocity = new Velocity();
+
+	/**
+	 * The Player's input.
+	 *
+	 * @var {Object} Default from Game.
+	 */
+	input = Game.Input;
+
+	/**
+	 * The Player's velocity.
+	 *
+	 * @var {Object} Default from Game.
+	 */
+	pressed = this.input.pressed;
+
+	/**
 	 * Construct the Player.
 	 *
 	 * @param {Array}    group
 	 * @param {Position} position
 	 * @param {Size}     size
 	 */
-	constructor( group = [], position = { x: 0, y: 0 }, size = { w: 1, h: 1 } ) {
+	constructor(
+		group    = [],
+		position = { x: 0, y: 0 },
+		size     = { w: 1, h: 1 }
+	) {
 		super( group, position, size, 'Yellow' );
 
 		this.set();
@@ -47,6 +107,9 @@ export default class Player extends Tile {
 
 		// Contact.
 		this.contact = new Contact();
+
+		// Orientation.
+		this.orientation = new Orientation( 90, 0 );
 
 		// Velocity.
 		this.velocity = new Velocity( 0, 0 );
@@ -203,6 +266,8 @@ export default class Player extends Tile {
 		if ( this.pressed( Settings.input.left ) ) {
 			this.orientation.x = 270;
 		}
+
+		this.orientation.y = 0;
 	}
 
 	/**
@@ -251,10 +316,15 @@ export default class Player extends Tile {
 	/**
 	 * Collide with solid Tiles (Platforms, Walls, etc...)
 	 *
-	 * @param {Velocity} velocity
+	 * @param   {Velocity} velocity
 	 * @returns {Void}
 	 */
-	collide = ( velocity = { x: 0, y: 0 } ) => {
+	collide = (
+		velocity = {
+			x: 0,
+			y: 0
+		}
+	) => {
 
 		// Default.
 		let p = Game.Room.tiles.platforms.concat(
