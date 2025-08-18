c2ef3b328aaa194a56994f1cfca7233f15857c3e
John James Jacoby <johnjamesjacoby@me.com>
2025-08-17 22:23:16 -0500

    Export timer.js

--- scripts/core/utilities/exports.js
+++ scripts/core/utilities/exports.js
@@ -3,3 +3,4 @@ export { default as Draw }   from './draw.js';
 export { default as Easing } from './easing.js';
 export { default as Hooks }  from './hooks.js';
 export { default as Jobs }   from './jobs.js';
+export { default as Timer }  from './timer.js';
